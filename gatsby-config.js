0c425a9e3c591461dc0a01e1bcaa770848695a5d
jaegoon <newenws@naver.com>
2019-03-27 23:30:16 +0900

    footer edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,7 +129,7 @@ module.exports = {
     'gatsby-plugin-sharp',
     {
       resolve: 'gatsby-plugin-google-analytics',
-      options: { trackingId: 'UA-73379983-2' },
+      options: { trackingId: 'UA-137052901-1' },
     },
     {
       resolve: 'gatsby-plugin-google-fonts',
